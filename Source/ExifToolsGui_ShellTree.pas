f4346e68a4dfdc6ae4ed040d36509aaf880e0c96
FrankBijnen <63312605+FrankBijnen@users.noreply.github.com>
2025-07-02 16:18:29 +0200

    auto refresh for file manager as well as better cut and paste support
     #911

--- Source/ExifToolsGui_ShellTree.pas
+++ Source/ExifToolsGui_ShellTree.pas
@@ -10,11 +10,12 @@ uses System.Classes, System.SysUtils,
      Vcl.Shell.ShellCtrls, Vcl.ComCtrls,
      ExifToolsGUI_MultiContextMenu;
 
+const
+  sfsNeedsCheck = -2;
+
 type
-  TSubFolderStateIndex = (sfsNeedsCheck = -2, sfsInitial = -1);
   TShellTreeView = class(Vcl.Shell.ShellCtrls.TShellTreeView, IShellCommandVerbExifTool)
   private
-    FDeferSubFolderCheck: boolean;
     FPreferredRoot: string;
     FPaths2Refresh: TStringList;
     ICM2: IContextMenu2;
@@ -51,7 +52,6 @@ type
     property OnEditingEnded: TTVEditedEvent read FOnEditingEnded write FOnEditingEnded;
     property Path: string read GetPath write SetPath;
     property PreferredRoot: string read FPreferredRoot write FPreferredRoot;
-    property DeferSubFolderCheck: boolean read FDeferSubFolderCheck write FDeferSubFolderCheck;
  end;
 
 implementation
@@ -73,7 +73,6 @@ begin
   FPaths2Refresh.Duplicates := TDuplicates.dupIgnore;
   ICM2 := nil;
   FPreferredRoot := '';
-  FDeferSubFolderCheck := false;
 end;
 
 destructor TShellTreeView.Destroy;
@@ -109,49 +108,82 @@ end;
 
 procedure TShellTreeView.InitNode(NewNode: TTreeNode; ID: PItemIDList; ParentNode: TTreeNode);
 var
+  CanAdd: Boolean;
   NewFolder: IShellFolder;
-  TempFolder: TShellFolder;
+  AFolder: TShellFolder;
+  NewShellFolder: TShellFolder;
 begin
-  // Archives (TAR, ZIP TGZ etc.) may be reported as folders in TCustomShellTreeView.PopulateNode
-  // Resulting in needlessly scanning large files
-  if not (otNonFolders in ObjectTypes) then
+  // Performance optimization only for directories.
+  if (otNonFolders in ObjectTypes) then
   begin
-    NewFolder := GetIShellFolder(TShellFolder(ParentNode.Data).ShellFolder, ID);
-    TempFolder := TShellFolder.Create(TShellFolder(ParentNode.Data), ID, NewFolder);
-    try
-      if not (HasDirAttribute(TempFolder.PathName)) then
-      begin   // TempFolder does not have Directory Attribute. Dont show in Treeview
-        NewNode.Delete;
-        exit;
-      end;
-    finally
-      TempFolder.Free;
-    end;
-    if (FDeferSubFolderCheck) then
-      NewNode.StateIndex := Ord(TSubFolderStateIndex.sfsNeedsCheck); // Dont call 'SubFolders' on create
+    inherited InitNode(NewNode, ID, ParentNode);
+    exit;
   end;
 
-  inherited InitNode(NewNode, ID, ParentNode);
+  AFolder := TShellFolder(ParentNode.Data);
+  NewFolder := GetIShellFolder(AFolder.ShellFolder, ID);
+  NewNode.Data := TShellFolder.Create(AFolder, ID, NewFolder);
+  NewShellFolder := TShellFolder(NewNode.Data);
+
+  // Archives (TAR, ZIP TGZ etc.) may be reported as folders in TCustomShellTreeView.PopulateNode
+  // Resulting in needlessly scanning large files
+  if (HasDirAttribute(NewShellFolder.PathName) = false) then
+    NewNode.Delete // AFolder does not have Directory Attribute. Dont show in Treeview
+  else
+  begin
+    NewNode.Text := NewShellFolder.DisplayName;
+    // Use StateIndex as a flag to indicate that this node needs to be examined
+    // StateIndex is not used in TShellTreeview
+    NewNode.StateIndex := sfsNeedsCheck;
+    // Assume the node has Children. Will be set later correctly.
+    // Needed for SetPathFromId to work
+    NewNode.HasChildren := true;
+
+    CanAdd := True;
+    if Assigned(OnAddFolder) then
+       OnAddFolder(Self, NewShellFolder, CanAdd);
+    if not CanAdd then
+      NewNode.Delete;
+  end;
+
 end;
 
 function TShellTreeView.CustomDrawItem(Node: TTreeNode; State: TCustomDrawState;
   Stage: TCustomDrawStage; var PaintImages: Boolean): Boolean;
 var
   prc: Trect;
+  AFolder: TShellFolder;
 begin
-  result := inherited CustomDrawItem(Node, State, Stage, PaintImages);
-
-  if (FDeferSubFolderCheck) and
+  if not (otNonFolders in ObjectTypes) and // Performance optimization only for directories.
      (Stage = TCustomDrawStage.cdPrePaint) and
      (Node.Data <> nil) and
-     (Node.StateIndex < Ord(TSubFolderStateIndex.sfsInitial)) then
+     (Node.StateIndex = sfsNeedsCheck) then
   begin
-    if TreeView_GetItemRect(Handle, Node.ItemId, prc, false) then  // Inview ?
+    if TreeView_GetItemRect(Handle, Node.ItemId, prc, false) then // Only check items in view
     begin
-      Node.StateIndex := Ord(TSubFolderStateIndex.sfsInitial);
+      // Only do the check 1 time.
+      Node.StateIndex := Node.StateIndex + 1;
+
+      // Get Folder
+      AFolder := TShellFolder(Node.Data);
+
+      // Image and selected index
+      // Dont care for SHGFI_OPENICON, rarely used
+      if UseShellImages and
+         not Assigned(Images) then
+      begin
+        Node.ImageIndex := AFolder.ImageIndex(false);
+        Node.SelectedIndex := AFolder.ImageIndex(false);
+      end;
+
+      // Has subfoldere?
       Node.HasChildren := TShellFolder(Node.Data).SubFolders;
+
+      // Dont care if the folder is shared, or has non folder subitems
     end;
   end;
+
+  result := inherited CustomDrawItem(Node, State, Stage, PaintImages);
 end;
 
 procedure TShellTreeView.DoContextPopup(MousePos: TPoint; var Handled: boolean);
@@ -170,7 +202,9 @@ begin
 
     ShowMultiContextMenu(MousePos);
 
-  //  inherited;
+// Dont call inherited
+//  inherited;
+
     if Assigned(FOnAfterContextMenu) then
       FOnAfterContextMenu(Self);
   finally
